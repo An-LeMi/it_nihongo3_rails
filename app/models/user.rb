5cb52eff7d7522f20b0e69384bcbe9a6fac19f10
An-LeMi <leminhan18@gmail.com>
2021-12-03 09:47:17 +0000

    Add avatar on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     extend Devise::Models
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    has_one_attached :avatar
 end
